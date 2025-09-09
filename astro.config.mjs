8d41c53f0c172faa0bb7b5c6a8fcf5d87690d6fb
amarobuk <amontero@buk.cl>
2025-09-08 23:12:15 -0300

    feat: supabase connection and little example with characters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    env: {
+        schema: {
+            SUPABASE_KEY: envField.string({
+                context: 'server',
+                access: 'public'
+            }),
+        }
+    }
+});
