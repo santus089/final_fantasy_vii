6407cab658a2931f634dcff4e9b85cb4a00c05bd
santus089 <danielsaldivargonzalez@gmail.com>
2025-09-10 03:20:18 -0300

    feat: instalation tailwind and create navbar (provisional)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    env: {
-        schema: {
-            SUPABASE_KEY: envField.string({
-                context: 'server',
-                access: 'public'
-            }),
-        }
-    }
+  env: {
+      schema: {
+          SUPABASE_KEY: envField.string({
+              context: 'server',
+              access: 'public'
+          }),
+      }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
